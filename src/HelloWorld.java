1bb86b68cd017f2175f31fd1a548803d3de7ff49
lalwani1312 <hitesh.lalwani92@gmail.com>
2017-10-07 20:25:22 +0530

    The mandatory "Hello World" program

---
+++ src/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * Created by hitesh-lalwani on 7/10/17.
+ *
+ * Hello World program
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
